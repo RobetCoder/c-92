1e82b834dfcb168410b96e4f13ed9733d832af48
RobetCoder <93022804+RobetCoder@users.noreply.github.com>
2022-05-10 13:15:52 +0530

    Add files via upload

---
+++ kwitter_room.js.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDIYEMHrUwzFiHTq03pJrQI0vZ8CLJXv1I",
+  authDomain: "project-90883.firebaseapp.com",
+  projectId: "project-90883",
+  storageBucket: "project-90883.appspot.com",
+  messagingSenderId: "919919289681",
+  appId: "1:919919289681:web:9849dfd3c5761c4b53df70"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
